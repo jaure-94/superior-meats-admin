a1043c8673415f4cafd6c492c20109b86ce5f789
Tawanda Jaure <tawandajaujau@gmail.com>
2026-09-15 19:13:51 +0200

    fixed product addition functionality

---
+++ src/firebase.jsx
@@ -0,0 +1,30 @@
+const VITE_FIREBASE_APP_API_KEY = import.meta.env.VITE_FIREBASE_APP_API_KEY;
+const VITE_FIREBASE_APP_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_APP_AUTH_DOMAIN;
+const VITE_FIREBASE_APP_PROJECT_ID = import.meta.env.VITE_FIREBASE_APP_PROJECT_ID;
+const VITE_FIREBASE_APP_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_APP_STORAGE_BUCKET;
+const VITE_FIREBASE_APP_MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_APP_MESSAGING_SENDER_ID;
+const VITE_FIREBASE_APP_APP_ID = import.meta.env.VITE_FIREBASE_APP_APP_ID;
+
+import { initializeApp } from "firebase/app";
+
+const hasFirebaseConfig = [
+  VITE_FIREBASE_APP_API_KEY,
+  VITE_FIREBASE_APP_AUTH_DOMAIN,
+  VITE_FIREBASE_APP_PROJECT_ID,
+  VITE_FIREBASE_APP_STORAGE_BUCKET,
+  VITE_FIREBASE_APP_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_APP_ID,
+].every(Boolean);
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_APP_API_KEY,
+  authDomain: VITE_FIREBASE_APP_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_APP_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_APP_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_APP_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_APP_ID
+};
+
+// Initialize Firebase
+export const firebaseApp = hasFirebaseConfig ? initializeApp(firebaseConfig) : null;
